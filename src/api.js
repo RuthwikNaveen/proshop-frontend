966bab5410d26f45382f9b2584083541fbce78fa
RuthwikNaveen <ruthwiknaveen@gmail.com>
2025-09-27 12:40:26 +0530

    Feat: Use explicit API endpoint for production

--- src/api.js
+++ src/api.js
@@ -1,14 +1,7 @@
 import axios from 'axios';
 
-// THIS IS THE CRITICAL STEP:
-// Paste the live backend URL you just copied from Render here.
-const API_ENDPOINT = 'https://proshop-backend-2lo3.onrender.com';
-
 const api = axios.create({
-  // This line of code is the "brain".
-  // If the website is in "production" (live on the internet), it uses your Render URL.
-  // Otherwise (on your computer), it uses a relative path, which works with the proxy.
-  baseURL: process.env.NODE_ENV === 'production' ? API_ENDPOINT : '',
+  baseURL: process.env.REACT_APP_API_ENDPOINT || '',
 });
 
 export default api;
